eff8750764df20a155b482e967d71771711793bb
ViktoriiaMurzhak <viktoria.murzhak@gmail.com>
2023-03-13 21:56:39 +0000

    Deploying to gh-pages from @ ViktoriiaMurzhak/vr-zone@e6c80847794cbabcb24e750b23d80d2b4c316437 🚀

---
+++ static/js/335.a040e5f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[335],{335:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(791);var n=r(184);function u(){return(0,n.jsx)("div",{children:"AboutUs"})}}}]);
+//# sourceMappingURL=335.a040e5f5.chunk.js.map
---
+++ static/js/521.589f9905.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[521],{521:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});r(791);var n=r(184);function c(){return(0,n.jsx)("div",{children:"Service"})}}}]);
+//# sourceMappingURL=521.589f9905.chunk.js.map

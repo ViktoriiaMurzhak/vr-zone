ffd66ab0245d610eac5f30954198738d8e46f394
Viktoriia <viktoria.murzhak@gmail.com>
2023-03-22 01:52:31 +0200

    add animation hero

--- src/pages/HomePage/Home.styled.js
+++ src/pages/HomePage/Home.styled.js
@@ -1,10 +1,22 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import heroBg from '../../helpers/images/hero-bg.png';
 import { breakpoints } from 'helpers/variables';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 export const HeroBox = styled.div`
   background: transparent;
   padding: 50px 0 50px 0;
+  animation: ${fadeIn} 1s ease-out;
 
   @media ${breakpoints.minTablet} {
     display: block;
